a9b494012b9c5dbf7d62ecafad0d322e22b97d69
Kwarthys <Kwarthys@gmail.com>
2018-03-12 22:48:41 +0100

    UI Farm added. Script for data lookup on production buildings done.

---
+++ Assets/Scripts/BCanvasScripts/ResourceUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceUI : MonoBehaviour {
+
+    [SerializeField]
+    Text productionText;
+    [SerializeField]
+    ProductionType resourceType;
+
+    // Use this for initialization
+    void Update () {
+        StringBuilder builder = new StringBuilder();
+        productionText.text = builder.Append("Producing : ").Append(GetComponent<Building>().GetProd(1).ToString("N0")).Append(" ").Append(resourceType.ToString()).ToString();
+	}
+}
