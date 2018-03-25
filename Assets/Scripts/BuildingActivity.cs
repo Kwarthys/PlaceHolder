050c9ac9bf58be12aff6906a707affb97172abc4
Kwarthys <Kwarthys@gmail.com>
2018-03-25 11:23:39 +0200

    Building activity rendered by ParticleSystem activation and element rotation.

---
+++ Assets/Scripts/BuildingActivity.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingActivity : MonoBehaviour {
+
+    [SerializeField]
+    List<GameObject> toToggle = new List<GameObject>();
+
+    public bool activated = false;
+
+    private void Update()
+    {
+        Building b = GetComponent<Building>();
+
+        if(b != null)
+        {
+            if(activated && b.recruitment.Count == 0)
+            {
+                deactivate();
+            }
+            else if(!activated && b.recruitment.Count > 0)
+            {
+                activate();
+            }
+        }
+    }
+
+    private void Start()
+    {
+        toggle();
+    }
+
+    void activate () {
+        activated = true;
+
+        toggle();
+	}
+
+    void deactivate()
+    {
+        activated = false;
+
+        toggle();
+    }
+
+    void toggle()
+    {
+        foreach (GameObject g in toToggle)
+        {
+            g.SetActive(activated);
+        }
+    }
+}
